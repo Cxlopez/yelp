b8496b82ae1fec4f49cf165a05797a3f36ac38f8
Cxlopez <cristian4402@hotmail.com>
2023-06-26 16:12:56 -0400

    setup put request

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/getResaurants", (req, res) => {
+  res.json({
+    status: "sucess",
+    restaurant: "mcdonalds"
+  });
+})
+
 const port = process.env.PORT || 4001;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
