b28f276ad5084c0d815f627a4478975f91bbdf13
Cxlopez <cristian4402@hotmail.com>
2023-06-27 11:12:26 -0400

    trying middlewares

--- server/server.js
+++ server/server.js
@@ -1,15 +1,39 @@
 require("dotenv").config();
 const express = require("express");
-
-
+const morgan = require("morgan")
 const app = express();
 
-app.get("/getResaurants", (req, res) => {
-  res.json({
+//middlewares 
+app.use(morgan("dev"));
+
+// app.use((req, res, next) => {
+//   console.log("I'm your middleware!");
+//   next();
+// }) <-- cusotm middleware 
+
+// Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  console.log("route handler ran");
+  res.status(200).json({
     status: "sucess",
-    restaurant: "mcdonalds"
+    data: {
+      restaurant: ["mcdonalds", "wendys"],
+
+    }
   });
 })
+
+// Get a restaurant
+app.get("api/v1/restaurants/:restaurantid", (req, rest) => {
+  console.log(req.params);
+})
+
+//Create a restaurant
+
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req);
+})
+
 
 const port = process.env.PORT || 4001;
 app.listen(port, () => {
