3e6b2f83879f266d7f3ec8d3dd2de4902367cdcd
Cxlopez <cristian4402@hotmail.com>
2023-07-03 16:52:56 -0400

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetail from "./routes/RestaurantDetail";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        <Route path="/restaurants/:id" element={<RestaurantDetail />} />
+      </Routes>
     </div>
   );
 }
