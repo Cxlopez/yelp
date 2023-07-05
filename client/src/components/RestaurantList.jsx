49ab506714994aa5f0df0fe9f56ecab003c0138b
Cxlopez <cristian4402@hotmail.com>
2023-07-04 20:02:01 -0400

    create header,AddRes,ResList components

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RestaurantList() {
+  return (
+    <div>
+      <h1 className='font-weight-light dislay -1 text-center'>
+        Restaurant Finder
+      </h1>
+    </div>
+  )
+}
+
+export default RestaurantList
