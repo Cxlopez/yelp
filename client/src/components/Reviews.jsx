4eee3c48f6746e55dbe9cfc7f9fb4825b8bdeb36
Cxlopez <cristian4402@hotmail.com>
2023-08-29 11:50:02 -0400

    create reviews component

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Reviews() {
+  return (
+    <div>
+      <div className="card text-white-bg-primary mb-3 mr-4"></div>
+    </div>
+  )
+}
+
+export default Reviews
