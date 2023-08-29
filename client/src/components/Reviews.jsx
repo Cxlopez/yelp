7fcc91bcce65f35bb9a6ab5d7d37308229681649
Cxlopez <cristian4402@hotmail.com>
2023-08-29 11:54:37 -0400

    add styling & hard code to reviews

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import StarRating from './StarRating'
 
 function Reviews() {
   return (
-    <div>
-      <div className="card text-white-bg-primary mb-3 mr-4"></div>
+    <div className='row row-cols-3 mb-2'>
+      <div className="card text-white-bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+        <div className="card-header d-flex justifu-content-between">
+          <span>Estuardo</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This place was great</p>
+        </div>
+      </div>
     </div>
   )
 }
