211989cf0795248224165dad70fbe49779b94e8f
Cxlopez <cristian4402@hotmail.com>
2023-09-09 16:17:13 -0400

    destructure and map reviews & add key

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import StarRating from './StarRating'
+import React from "react";
+import StarRating from "./StarRating";
 
-function Reviews() {
+function Reviews({ reviews }) {
   return (
-    <div className='row row-cols-3 mb-2'>
-      <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+    <div className="row row-cols-3 mb-2">
+      {reviews.map((review) => {
+        return (
+          <div
+            key={review.id}
+            className="card text-white bg-primary mb-3 mr-4"
+            style={{ maxWidth: "30%" }}
+          >
+            <div className="card-header d-flex justifu-content-between">
+              <span>{review.name}</span>
+              <span>
+                <StarRating rating={review.rating} />
+              </span>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{review.review}</p>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
         <div className="card-header d-flex justifu-content-between">
           <span>Estuardo</span>
           <span>
@@ -14,9 +33,9 @@ function Reviews() {
         <div className="card-body">
           <p className="card-text">This place was great</p>
         </div>
-      </div>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default Reviews
+export default Reviews;
--- client/src/routes/RestaurantDetail.jsx
+++ client/src/routes/RestaurantDetail.jsx
@@ -29,7 +29,7 @@ function RestaurantDetail() {
     <div>{selectedRestaurant && (
       <>
       <div className="mt-3">
-        <Reviews reviews={selectedRestaurant}/>
+        <Reviews reviews={selectedRestaurant.reviews}/>
       </div>
       <AddReview />
       </>
